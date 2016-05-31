fa49a0a53373571c0f5ea58ae5fcaa0ba16d846a
GitTestingDemo <Joyce10302@gmail.com>
2016-05-31 12:46:43 +0800

    Upload ATMDemo code to git server

---
+++ ATMDemo/src/ATMException.java
@@ -0,0 +1,12 @@
+//ATM异常类
+public class ATMException extends Exception{
+	String msg; //异常消息
+	ATMException(String msg)
+	{
+		this.msg = msg;
+	}
+	public String toString()
+	{
+		return this.msg;
+	}
+}
